1c171364980aa078398f872f133e7c79b1591a21
coll2k <collovenseri1@gmail.com>
2026-09-16 00:20:49 +0100

    adding logo instead of hyperlink

--- scripts/lastfm.js
+++ scripts/lastfm.js
@@ -9,7 +9,6 @@ async function getLastTrack() {
 
     const track = data.recenttracks.track[0];
     const statusEl = document.getElementById("lastfm-status");
-    const footerEl = document.getElementById("lastfm-footer");
 
     if (!track) {
         statusEl.textContent = "No recent tracks found.";
@@ -23,8 +22,6 @@ async function getLastTrack() {
     statusEl.textContent = nowPlaying
         ? `now playing: ${name.toLowerCase()} by ${artist.toLowerCase()}`
         : `last played: ${name.toLowerCase()} b ${artist.toLowerCase()}`;
-
-     footerEl.innerHTML = `powered by Last.fm -> <a href="https://www.last.fm/user/${USERNAME}" target="_blank">visit my profile</a>`;
 }
 
 getLastTrack();
